d7b842fb5ecd0107d131f417ab9f05d7658451fb
MuhammadJunaid948 <137820226+MuhammadJunaid948@users.noreply.github.com>
2023-11-26 11:24:30 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,147 @@
+// *******************Quetion no 1*************
+
+// let myArr = [];
+
+// ********************Questiion no 2*************
+
+// const studentsName = {
+//     students:[]
+// }
+
+// studentsName.students.push(1,2,3,4,5)
+// console.log(studentsName)
+
+
+// ********************Question no 3**********
+
+// const stringArr = ["Junaid","Jawad","Waqas"]
+// console.log(stringArr[])
+
+
+// ********************Quetion no 4*************************
+
+// const numberArr = [1,2,3,4,5,6,7,8,]
+
+// console.log(numberArr)
+
+
+// *********************Question no 5***************
+
+// const boolean_array = [name = true];
+// console.log(boolean_array)
+
+// *********************Question no 6******************
+
+// const mixedArray = ["Junaid","Batman",205, name = false]
+// console.log(mixedArray[3]);
+
+// ********************Question no 7******************
+
+// let education_Qualificatiion = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"];
+
+// document.write("<h1>Qualification</h1>" + "<br>")
+// document.write(education_Qualificatiion[0] + "<br>")
+// document.write(education_Qualificatiion[1] + "<br>")
+// document.write(education_Qualificatiion[2] + "<br>")
+// document.write(education_Qualificatiion[3] + "<br>")
+// document.write(education_Qualificatiion[4] + "<br>")
+// document.write(education_Qualificatiion[5] + "<br>")
+// document.write(education_Qualificatiion[6] + "<br>")
+// document.write(education_Qualificatiion[7] + "<br>")
+
+
+
+// *************************Question no 8*****************
+
+// const students = ["Michael", "John", "Tony"]
+// const studentsMarks = [450, 480, 490]
+
+// const max_marks = 500;
+
+// const percent = (studentsMarks / max_marks) * 100;
+
+// document.write(students[0] + "  marks = " + studentsMarks[0] + ":  percentages is: " + percent[0] + "<br>")
+// document.write(students[1] + " marks = " + studentsMarks[1] + ":  percentages is: " + percent[1] + "<br>")
+// document.write(students[2] + " marks = " + studentsMarks[2] + ":  percentages is: " + percent[2] + "<br>")
+
+// **************************Question no 10*******************
+
+// I have tried it so times but I don't get it what to do  
+// **************************Question no 10*******************
+
+// const scores = [320,230,480,120]
+// scores.sort()
+
+// console.log(scores)
+
+// **************************Question no 11*******************
+
+// let cityNames = ["karachi","Islamabad","lahore","quetta","peshawar"]
+
+// let newCities = cityNames.slice(1,3);
+// document.write("<h1> Cities List </h1>")
+// document.write(cityNames + "<br>")
+// document.write("<h1> Selected List </h1>")
+// document.write(newCities + "<br>")
+
+// **************************Question no 12*******************
+
+// const singleArr = ["This","is","my","car"]
+
+// const newArr = singleArr.join()
+
+// console.log(newArr)
+
+// **************************Question no 13*******************
+
+// const myNewArr = []
+// myNewArr.push("keyboard")
+// myNewArr.push("Mouse")
+// myNewArr.push("Pointer")
+// myNewArr.push("Monitor")
+// console.log(myNewArr.shift())
+// console.log(myNewArr.shift())
+// console.log(myNewArr.shift())
+
+// **************************Question no 14*******************
+
+// const myNewArr = []
+// myNewArr.push("keyboard")
+// myNewArr.push("Mouse")
+// myNewArr.push("Pointer")
+// myNewArr.push("Monitor")
+// console.log(myNewArr.pop())
+// console.log(myNewArr.pop())
+// console.log(myNewArr.pop())
+// console.log(myNewArr.pop())
+
+// **************************Question no 15*******************
+
+// const newArr = ["Apple","Samsung", "Motorola", "Nokia", "Sony", "Haier"]
+// document.write("<h1>Phone Manufacturer</h1>")
+// document.write(newArr[0] + "<br>" )
+// document.write(newArr[1] + "<br>" )
+// document.write(newArr[2] + "<br>" )
+// document.write(newArr[3] + "<br>" )
+// document.write(newArr[4] + "<br>" )
+// document.write(newArr[5] + "<br>" )
+// document.write(newArr[6] + "<br>" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
